2b2d04857684f9896554e1bd66828b0d470fc1dc
PeterCypers <peter-cypers@hotmail.com>
2026-03-02 14:56:08 +0100

    03 "What is MVC?" completed

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // return view('welcome');
-    // return view('test');
-    return view('home');
-})->name('dashboard');
+// Route::get('/', function () {
+//     // return view('welcome');
+//     // return view('test');
+//     return view('home');
+// })->name('dashboard');
+
+// controller class and the method -> route goes through controller now
+Route::get('/', [ChirpController::class, 'index']);
 
 Route::get('test/{test}', [TestController::class, 'test'])->name('test');
